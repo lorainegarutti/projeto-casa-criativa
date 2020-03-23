9ae2daa957c93151805d3919d22c007b8c37b20b
Loraine Garutti <60954622+lorainegarutti@users.noreply.github.com>
2020-03-23 15:37:26 -0300

    primeira parte

---
+++ index.js
@@ -0,0 +1,8 @@
+const botaoAdicionarIdeia = document
+                                .querySelector("button.adicionarIdeia")
+                                .addEventListener("click", function() {
+                                    document
+                                        .querySelector("#footer")
+                                        .classList
+                                        .toggle("hide")
+                                })
